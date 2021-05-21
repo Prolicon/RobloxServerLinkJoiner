f66fe933b640fbed67e013eef26e3199ade01490
Sav <kiritotheilluminatidorito@gmail.com>
2021-05-21 01:32:24 +0100

    Update and rename JoinLink.user.js to TamperMonkey.user.js

--- TamperMonkey.user.js
+++ TamperMonkey.user.js
@@ -3,10 +3,10 @@
 // @version  1.337
 // @grant    none
 // @run-at   document-idle
-// @match    https://*.roblox.com/games/*
+// @match    https://*.roblox.com/*
 // @author   Prolikon
 // ==/UserScript==
 
 var placeid = window.location.href.match(/games\/(\d+)\//)[1];
 var gameid = window.location.href.match(/\?gameid=([^\/]+)/)[1];
-unsafeWindow.Roblox.GameLauncher.joinGameInstance(placeid, gameid);
+Roblox.GameLauncher.joinGameInstance(placeid, gameid);
